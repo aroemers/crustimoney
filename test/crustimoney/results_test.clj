e67b21e7c1b3dd9eb864d6263960d9c10f94c560
Arnout Roemers <roemers@zorgdomein.nl>
2023-03-19 00:25:31 +0100

    Add results test
    Only the more complicated. The others are tested in all the other unit tests.

---
+++ test/crustimoney/results_test.clj
@@ -0,0 +1,14 @@
+(ns crustimoney.results-test
+  (:require [clojure.set :as set]
+            [clojure.test :refer [deftest is]]
+            [crustimoney.results :as r]))
+
+(deftest errors->line-column-test
+  (let [text     "foo\n\nbar"
+        expected #{{:at 0 :line 1 :column 1}
+                   {:at 2 :line 1 :column 3}
+                   {:at 3 :line 1 :column 4}
+                   {:at 4 :line 2 :column 1}
+                   {:at 5 :line 3 :column 1}
+                   {:at 8 :line 3 :column 4}}]
+    (is (= expected (r/errors->line-column text (set/project expected [:at]))))))
