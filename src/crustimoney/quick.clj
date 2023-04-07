03f1c9b9819c8036a78b99de904f8dd9b9bd2e14
Arnout Roemers <roemers@zorgdomein.nl>
2023-04-08 01:37:06 +0200

    Better docs, built-ins available, nil on error

--- src/crustimoney/quick.clj
+++ src/crustimoney/quick.clj
@@ -1,23 +1,34 @@
 (ns crustimoney.quick
-  (:require [crustimoney.core :as core]
+  "A namespace for those quick \"I'd rather use something better than a
+  regex\"-moments."
+  (:require [crustimoney.built-ins :as built-ins]
+            [crustimoney.combinators :as c]
+            [crustimoney.core :as core]
             [crustimoney.data-grammar :as data-grammar]
             [crustimoney.results :as r]
             [crustimoney.string-grammar :as string-grammar]))
 
 (defn parse
-  "Quickly parse `text` using the string or data parser `definition`.
+  "Quickly parse `text` using the string- or data parser `definition`.
+  The definition cannot be recursive. The predefined parsers in the
+  `built-ins` namespace are available.
+
   A success result is transformed such that the matched texts are
   directly available. For example:
 
-      (crusti \"'alice' (' and ' (:who [a-z]+))+\"
+      (crusti \"'alice' (' and ' (:who word))+\"
               \"alice and bob and eve\")
 
       => [nil \"alice and bob and eve\"
           [:who \"bob\"]
-          [:who \"eve\"]]"
+          [:who \"eve\"]]
+
+  When the result is an error, nil is returned."
   [definition text]
-  (let [parser (if (string? definition)
-                 (string-grammar/create-parser definition)
-                 (data-grammar/create-parser definition))
-        result (core/parse parser text)]
-    (cond->> result (r/success? result) (r/success->texts text))))
+  (let [rules (c/grammar built-ins/all
+               {:root (if (string? definition)
+                        (string-grammar/create-parser definition)
+                        (data-grammar/create-parser definition))})
+        result (core/parse (:root rules) text)]
+    (when (r/success? result)
+      (r/success->texts text result))))
